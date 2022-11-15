e0845f5bb73c32b68082ca8d9603a22bf67c4c30
hoin2 <jingyusin20@sookmyung.ac.kr>
2022-11-15 16:17:15 +0900

    07-1

--- main.c
+++ main.c
@@ -3,18 +3,18 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+char*proverb="All that glisters is not gold.";
+void setPointer(char*q);
 
 int main(void) {
-	int i=100;
-	int *p=&i;
-	int **q=&p;
-	
-	*p=200;
-	printf("i=%d,*p=%d,**q=%d\n",i,*p,**q);
-	
-	**q=300;
-	printf("i=%d,*p=%d,**q=%d\n",i,*p,**q);
-	
+	char*p="zzz";
+	setPointer(p);
+	printf("%s\n",p);
+	 
 	return 0;
+}
+
+void setPointer(char*q){
+	q=proverb;
 }
 
