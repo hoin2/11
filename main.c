390680cfcc80e4fc286c4e412df27ef4a6717ac6
hoin2 <jingyusin20@sookmyung.ac.kr>
2022-11-15 16:13:30 +0900

    02-2

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int i=4;
+	int i=300;
 	
 	int*pi=&i;
 	char*pc=&i;
